e223ed63b1a839b0178d0ed453ba0aedaa8b506a
Ivan Kristiyanov <ivan.kristiyanov@gmail.com>
2017-06-03 17:15:24 +0300

    Added spies and describe

--- server/server.test.js
+++ server/server.test.js
@@ -3,26 +3,33 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-    request(app)
-        .get('/')
-        .expect((res) => {
-            expect(res.body).toInclude({
-                error: 'Page not found.'
-            });
-        })
-        .end(done);
-});
+describe('Server tests', () => {
+    describe('Get /', () => {
+        it('should return hello world response', (done) => {
+            request(app)
+                .get('/')
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        error: 'Page not found.'
+                    });
+                })
+                .end(done);
+        });
+    });
 
-it('should contain user with name Some', (done) => {
-    request(app)
-        .get('/users')
-        .expect(200)
-        .expect((res) => {
-            expect(res.body).toInclude({
-                name: 'Some',
-                age: 25
-            });
-        })
-        .end(done);
+
+    describe('Get /users', () => {
+        it('should contain user with name Some', (done) => {
+            request(app)
+                .get('/users')
+                .expect(200)
+                .expect((res) => {
+                    expect(res.body).toInclude({
+                        name: 'Some',
+                        age: 25
+                    });
+                })
+                .end(done);
+        });
+    });
 });
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,52 +1,54 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var results = utils.add(4, 5);
+describe('Utils tests', () => {
+    it('should add two numbers', () => {
+        var results = utils.add(4, 5);
 
-    expect(results)
-        .toBeA('number')
-        .toBe(9);
-});
-
-it('should async add two numbers', (done) => {
-
-    utils.asyncAdd(3, 4, (sum) => {
-        expect(sum)
+        expect(results)
             .toBeA('number')
-            .toBe(7);
-        done();
+            .toBe(9);
     });
-});
 
-it('should return the square of a number', () => {
-    let res = utils.square(5);
-    expect(res)
-        .toBeA('number')
-        .toBe(25);
-});
+    it('should async add two numbers', (done) => {
 
-it('should async return the square of a number', (done) => {
-    utils.asyncSquare(9, (squared) => {
-        expect(squared)
-            .toBeA('number')
-            .toBe(81);
-        done();
-    });
-});
-
-it('should verify first and last names are set', () => {
-    var defaultObj = {
-        age: 25,
-        gender: "male"
-    };
-    var fullName = "John Smith";
-    var modifiedObj = utils.setName(defaultObj, fullName);
-
-    expect(modifiedObj)
-        .toBeA('object')
-        .toInclude({
-            firstName: fullName.split(' ')[0],
-            lastName: fullName.split(' ')[1]
+        utils.asyncAdd(3, 4, (sum) => {
+            expect(sum)
+                .toBeA('number')
+                .toBe(7);
+            done();
         });
+    });
+
+    it('should return the square of a number', () => {
+        let res = utils.square(5);
+        expect(res)
+            .toBeA('number')
+            .toBe(25);
+    });
+
+    it('should async return the square of a number', (done) => {
+        utils.asyncSquare(9, (squared) => {
+            expect(squared)
+                .toBeA('number')
+                .toBe(81);
+            done();
+        });
+    });
+
+    it('should verify first and last names are set', () => {
+        var defaultObj = {
+            age: 25,
+            gender: "male"
+        };
+        var fullName = "John Smith";
+        var modifiedObj = utils.setName(defaultObj, fullName);
+
+        expect(modifiedObj)
+            .toBeA('object')
+            .toInclude({
+                firstName: fullName.split(' ')[0],
+                lastName: fullName.split(' ')[1]
+            });
+    });
 });
